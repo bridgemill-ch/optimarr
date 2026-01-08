1d4c3cf14d39292bc20c30b35d945dcf75d61329
bridgemill-ch <server.bridgemill@gmail.com>
2026-01-08 20:50:44 +0100

    Fix: Add missing using directive for DatabaseFacade

--- Services/DatabaseMigrationService.cs
+++ Services/DatabaseMigrationService.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
